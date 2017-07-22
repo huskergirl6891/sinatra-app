d4a23ac039ab4564dc0695c9f3e6313f6eb94686
Carissa Castro <carissabcastro@gmail.com>
2017-07-22 17:52:10 +0000

    Finished part 1 of restful routes

--- routes.rb
+++ routes.rb
@@ -7,8 +7,31 @@ get '/users' do
   erb :"users/index"
 end
 
+get '/users/new' do
+  erb :"/users/new"
+end
+
+post '/users' do
+  @user = User.new
+  @user.name = params[:name]
+  @user.salutation = params[:salutation]
+  @user.email = params[:email]
+  @user.save
+  redirect "/users"
+end
+
 get '/users/:id' do
   id = params[:id].to_i
   @user = User.find(id)
-  erb :"users/show"
+  erb :"/users/show"
 end
+
+get '/users/:id/edit' do
+end
+
+patch '/users/:id' do
+end
+
+delete '/users/:id' do
+end
+
